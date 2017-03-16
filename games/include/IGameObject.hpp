c8ee2c2ce81f15d74d434e85899a62060cdf0f48
osborn_s <samuel.osborne@epitech.eu>
2017-03-16 11:14:31 +0100

    I gameObject so lucas can do the map.

---
+++ games/include/IGameObject.hpp
@@ -0,0 +1,29 @@
+/*
+** IGameObject.hpp for cpp_arcade in /home/osborn_s/Tek_2/cpp_arcade/IGameObject.hpp
+**
+** Made by Samuel Osborne
+** Login   <samuel.osborne@epitech.eu>
+**
+** Started on  Wed Mar 15 17:11:37 2017 Samuel Osborne
+** Last update Wed Mar 15 17:11:37 2017 Samuel Osborne
+*/
+
+#ifndef ARCADE_IGAMEOBJECT_HPP_
+# define ARCADE_IGAMEOBJECT_HPP_
+
+namespace 			arcade
+{
+  namespace			games
+  {
+    class 			IGameObject
+    {
+     public:
+      virtual			~IGameObject() {};
+
+      virtual arcade::Position 	getPos() const = 0;
+      virtual void 		setPos(const arcade::Position &pos) = 0;
+    };
+  }
+}
+
+#endif //ARCADE_IGAMEOBJECT_HPP_
