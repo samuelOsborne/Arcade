3e3c5be9425230d04bd8f91f60f20e7a45443b56
osborn_s <samuel.osborne@epitech.eu>
2017-03-29 11:44:14 +0200

    Added pacgum file.

---
+++ games/Pacman/include/Pacgum.hh
@@ -0,0 +1,34 @@
+/*
+** Pacgum.hh for cpp_arcade in /home/osborn_s/Tek_2/cpp_arcade/Pacgum.hh
+**
+** Made by Samuel Osborne
+** Login   <samuel.osborne@epitech.eu>
+**
+** Started on  Wed Mar 22 10:18:07 2017 Samuel Osborne
+** Last update Wed Mar 22 10:18:07 2017 Samuel Osborne
+*/
+
+#ifndef PACGUM_HH_
+# define PACGUM_HH_
+
+#include "Pickup.hpp"
+
+namespace 	arcade
+{
+  namespace 	games
+  {
+    class	Pacgum : public arcade::Pickup
+    {
+     public:
+      Pacgum(const uint16_t x, const uint16_t y);
+      Pacgum(const Pacgum& other);
+      Pacgum &operator=(const Pacgum&);
+      virtual ~Pacgum() {};
+
+      virtual std::string	getObj() const;
+      virtual void		setTileType();
+    };
+  };
+};
+
+#endif //PACGUM_HH
